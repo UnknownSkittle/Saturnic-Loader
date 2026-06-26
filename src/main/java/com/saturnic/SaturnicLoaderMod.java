20bbc160d298547ffecf0d8617fb174366c32b4e
Unknown_skittle <2011h32iscool@gmail.com>
2026-06-26 16:58:13 +0200

    Implement SaturnicLoaderMod with client initialization

--- src/main/java/com/saturnic/SaturnicLoaderMod.java
+++ src/main/java/com/saturnic/SaturnicLoaderMod.java
@@ -1,1 +1,30 @@
+package com.saturnic;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
+import java.nio.file.Path;
+
+public class SaturnicLoaderMod implements ClientModInitializer {
+
+    public static ScriptManager SCRIPT_MANAGER;
+
+    @Override
+    public void onInitializeClient() {
+        Path gameDir = FabricLoader.getInstance().getGameDir();
+        Path saturnicDir = gameDir.resolve("saturnic");
+
+        SCRIPT_MANAGER = new ScriptManager(saturnicDir);
+        SCRIPT_MANAGER.loadClient();
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player != null) {
+                SCRIPT_MANAGER.onTick();
+            }
+        });
+
+        System.out.println("[SaturnicLoader] Initialized.");
+    }
+}
 
