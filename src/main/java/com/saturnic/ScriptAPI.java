7834778972432e5a9f86ab813d6dadd60599be64
Unknown_skittle <2011h32iscool@gmail.com>
2026-06-26 16:58:56 +0200

    Create ScriptAPI class with ScriptManager dependency
    Added ScriptAPI class with a constructor for ScriptManager.

--- src/main/java/com/saturnic/ScriptAPI.java
+++ src/main/java/com/saturnic/ScriptAPI.java
@@ -1,1 +1,12 @@
+package com.saturnic;
 
+public class ScriptAPI {
+
+    private final ScriptManager manager;
+
+    public ScriptAPI(ScriptManager manager) {
+        this.manager = manager;
+    }
+
+    // TODO: expose player, world, packets, GUI, etc.
+}
