4f2512532741686125780ac1398ed59b3ac61364
Unknown_skittle <2011h32iscool@gmail.com>
2026-06-26 16:52:38 +0200

    Add files via upload

---
+++ src/client/java/name/saturnicloader/client/SaturnicLoaderClient.java
@@ -0,0 +1,10 @@
+package name.saturnicloader.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SaturnicLoaderClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
